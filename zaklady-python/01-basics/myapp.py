04e23ec55c27f4061460fa314d9a640e560ea0ab
Femmy :3 <118221254+MFrly@users.noreply.github.com>
2024-10-07 22:34:37 +0200

    Update myapp.py

--- zaklady-python/01-basics/myapp.py
+++ zaklady-python/01-basics/myapp.py
@@ -2,29 +2,30 @@ x = 10
 print("Zahrajeme si kvíz!!!")
 print("Víš kolik je 1 + 1?")
 print(":")
-answer=input()
-if answer == 2:
-    print("Ne tohle neni sprava odpoved")
-else:
-    print("Ne tohle neni sprava odpověď")
+answer = input()
 
-print("Zkuse něco podobného.")
-print("Když do dvou litrového kyblíku neleješ tři sta mililtrů vody.")
-print("Kolik máš kyblíků?")
-answer_2=input()
+if answer == "2":
+    print("Ano, to je správná odpověď!")
+else:
+    print("Ne, tohle není správná odpověď.")
+
+print("Zkusme něco podobného.")
+print("Když do dvou litrového kyblíku naliješ tři sta mililitrů vody, kolik máš kyblíků?")
+answer_2 = input()
+
 if int(answer_2) == 1:
-       print("Tohle už máš správně!!!!")
-       print("Dobře zkusme to naposledy.")
-       print("Napis cislo {}".format(x))
-       answer_3 = input()
-       if int(answer_3) == x:
-           print("No super!!!")
-           print("Nazdar!")
+    print("Tohle už máš správně!!!!")
+    print("Dobře, zkusme to naposledy.")
+    print(f"Napiš číslo {x}.")
+    answer_3 = input()
 
-       else:
-           print("Thole je konec")
+    if int(answer_3) == x:
+        print("No super!!!")
+        print("Nazdar!")
+    else:
+        print("Tohle je konec.")
 else:
-       print("Tohle bylo fakt jednoduché!!!")
-       print(answer_2)
+    print("Tohle bylo fakt jednoduché!!!")
+    print(answer_2)
 
 print("Tak někdy příště!!!")
